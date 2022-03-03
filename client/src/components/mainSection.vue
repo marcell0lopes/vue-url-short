2a4fd9596f7b727dfa6f7e89432dd09adfcfdf7f
Marcello Lopes <marcell0lopes@outlook.com>
2022-03-03 20:32:55 -0300

    main section component

---
+++ client/src/components/mainSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <main
+    class="w-screen h-screen bg-indigo-900 flex items-center justify-center"
+  >
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'mainSection',
+};
+</script>
