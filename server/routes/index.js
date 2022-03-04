06afc2f2cf0c0a6e5e29d25f94af3cd0f735f484
Marcello Lopes <marcell0lopes@outlook.com>
2022-03-04 16:07:06 -0300

    Add /top for display top 100 links sorted by clicks

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Url = require('../models/Url');
 
+// Get top 100 URL (by clicks)
+router.get('/top', async (req, res) => {
+  try {
+    const top100url = await Url.find().sort({ clicks: -1 }).limit(100);
+    res.json(top100url);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Internal Server Error');
+  }
+});
+
+// Redirect from short URL to original URL
 router.get('/:urlId', async (req, res) => {
   try {
     const url = await Url.findOne({ urlId: req.params.urlId });
