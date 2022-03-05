3bb3219eef8edd082818b0cbc9c9aff05a008159
Marcello Lopes <marcell0lopes@outlook.com>
2022-03-05 15:26:11 -0300

    moved top100 route

--- server/routes/urls.js
+++ server/routes/urls.js
@@ -4,6 +4,17 @@ const shortId = require('shortid');
 const Url = require('../models/Url');
 const utils = require('../utils/utils');
 
+// Get top 100 URL (by clicks)
+router.get('/top', async (req, res) => {
+  try {
+    const top100url = await Url.find().sort({ clicks: -1 }).limit(100);
+    res.json(top100url);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Internal Server Error');
+  }
+});
+
 // Gerar URL Reduzida:
 router.post('/short', async (req, res) => {
   const { originalUrl, urlTitle } = req.body;
