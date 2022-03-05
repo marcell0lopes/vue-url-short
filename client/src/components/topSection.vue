1086fe683023c748f8fe2f8e915c6ff5f51a04d0
Marcello Lopes <marcell0lopes@outlook.com>
2022-03-05 16:06:23 -0300

    add top 100 request

--- client/src/components/topSection.vue
+++ client/src/components/topSection.vue
@@ -7,23 +7,55 @@
       <small class="text-base tracking-normal text-gray-600">(por click)</small>
     </h2>
 
-    <div class="container mx-auto bg-indigo-900 p-4 rounded-lg shadow-lg">
+    <div
+      class="container max-w-4xl mx-auto bg-indigo-900 p-4 rounded-lg shadow-lg"
+    >
       <table class="table-auto w-full bg-white/80">
         <thead
-          class="text-left text-gray-500 bg-white/90 font-bold text-lg rounded-lg"
+          class="text-left text-gray-500 bg-white/90 font-bold text-sm rounded-lg"
         >
           <th class="p-4">Clicks</th>
           <th>Título</th>
           <th>Link</th>
         </thead>
-        <tbody class="text-lg font-bold">
-          <tr class="border-solid border-b-2 border-indigo-800">
-            <td class="p-4 text-indigo-500">0</td>
-            <td class="text-indigo-900">Meu Link</td>
-            <td>
-              <a class="underline text-indigo-500 font-normal"
-                >www.oi.com/8379128</a
+
+        <tbody class="text-md font-bold">
+          <tr v-if="isLoading" class="bg-indigo-900 py-10 w-full">
+            <td></td>
+            <td class="flex flex-col items-center justify-center col-span-3">
+              <h4 class="text-lg text-white/95 mt-4">
+                Carregando os 100 links mais acessados...
+              </h4>
+              <div
+                class="w-full pt-10 pb-20 relative flex items-center justify-center"
               >
+                <iframe
+                  src="https://giphy.com/embed/pf4xLNTrRFnYxJLYid"
+                  width="50px"
+                  height="50px"
+                  frameBorder="0"
+                  class="absolute"
+                />
+              </div>
+            </td>
+            <td></td>
+          </tr>
+
+          <tr
+            v-else
+            v-for="link in top100links"
+            :key="link._id"
+            class="border-solid border-b-2 border-indigo-800"
+          >
+            <td class="p-4 text-indigo-800">{{ link.clicks }}</td>
+            <td class="text-indigo-900">{{ link.urlTitle }}</td>
+            <td>
+              <a
+                :href="link.shortUrl"
+                class="text-teal-700 font-normal cursor-pointer hover:text-teal-900 underline transition-all"
+              >
+                {{ link.shortUrl }}
+              </a>
             </td>
           </tr>
         </tbody>
@@ -33,21 +65,21 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'topUrls',
   data() {
     return {
-      top100links: [
-        {
-          name: 'oi',
-          clicks: 2,
-        },
-        {
-          name: 'tchau',
-          clicks: 666,
-        },
-      ],
+      isLoading: true,
+      top100links: [],
     };
+  },
+  async created() {
+    await axios.get('http://localhost:3333/api/top').then((res) => {
+      this.top100links = res.data;
+      this.isLoading = false;
+    });
   },
 };
 </script>
